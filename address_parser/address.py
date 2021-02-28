908a186f96d048ea0843e0d7b855ea20235191f1
animishr <animesh.mishra1692@gmail.com>
2021-02-28 18:39:22 +0530

    Added docstrings

--- address_parser/address.py
+++ address_parser/address.py
@@ -8,7 +8,7 @@ from utils.standardize import standardize_direction, standardize_street_type
 class Address:
 
     def __init__(self, address):
-        self.address = address
+        self.input_address = address
         self.__labels = defaultdict(list)
         Address.__tag_components(self)
 
@@ -25,7 +25,7 @@ class Address:
         """
         Tags Address Component Labels to the tokens
         """
-        tokens = tokenize(self.address)
+        tokens = tokenize(self.input_address)
         current_tag = 'STOP'
 
         for token in tokens:
@@ -38,30 +38,72 @@ class Address:
 
     @property
     def house_num(self):
+        """
+        Gets House Number Component of the address.
+
+        :return: House Number
+        :rtype: str
+        """
         return ' '.join(self.__labels.get('house_num', ''))
 
     @property
     def pre_dir(self):
+        """
+        Gets Pre-Direction Component of the address.
+
+        :return: Pre-Direction
+        :rtype: str
+        """
         return ' '.join(self.__labels.get('pre_dir', ''))
 
     @property
     def str_nm(self):
+        """
+        Gets Street Name Component of the address.
+
+        :return: Street Name
+        :rtype: str
+        """
         return ' '.join(self.__labels.get('str_nm', ''))
 
     @property
     def str_type(self):
+        """
+        Gets Street Type Component of the address.
+
+        :return: Street Type
+        :rtype: str
+        """
         return ' '.join(self.__labels.get('str_type', ''))
 
     @property
     def post_dir(self):
+        """
+        Gets Post-Direction Component of the address.
+
+        :return: Post-Direction
+        :rtype: str
+        """
         return ' '.join(self.__labels.get('post_dir', ''))
 
     @property
     def occ_id(self):
+        """
+        Gets Occupancy Identifier Component of the address.
+
+        :return: Occupancy Identifier
+        :rtype: str
+        """
         return ' '.join(self.__labels.get('occ_id', ''))
 
     @property
     def occ_num(self):
+        """
+        Gets Occupancy Number Component of the address.
+
+        :return: Occupancy Number
+        :rtype: str
+        """
         return ' '.join(self.__labels.get('occ_num', ''))
 
     def __standardize_address(self):
@@ -82,10 +124,22 @@ class Address:
 
     @property
     def standardized_address(self):
+        """
+        Gets the Standardized Address.
+
+        :return: Standardized Address
+        :rtype: str
+        """
         return self.__standardize_address()
 
     @property
     def components(self):
+        """
+        Gets Components of the input address.
+
+        :return: Address Components
+        :rtype: dict
+        """
         tags = {}
         for tag in self.__labels:
             value = ' '.join(self.__labels.get(tag, ''))
